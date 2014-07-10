314f5bf8c59b1348ee73a7dd37c6518d4e9cf948
Amir Behnam <abehnam@uwaterloo.ca>
2014-07-10 14:10:04 -0400

    adding room sub objects to json

---
+++ Web/src/main/java/com/ece452/domain/MapperIgnore.java
@@ -0,0 +1,9 @@
+package com.ece452.domain;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD) 
+public @interface MapperIgnore {
+
+}
---
+++ Web/src/main/java/com/ece452/mapper/RoomAndSubObjectMapper.java
@@ -0,0 +1,30 @@
+package com.ece452.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.ece452.domain.Playlist;
+import com.ece452.domain.Room;
+import com.ece452.domain.User;
+
+public class RoomAndSubObjectMapper implements RowMapper<Room> {
+
+	@Override
+	public Room mapRow(ResultSet resultSet, int line) throws SQLException {
+		Room room = new Room();
+		try {
+			User user = new User();
+			user.map(resultSet);
+			room.setUser(user);
+			Playlist playlist = new Playlist();
+			playlist.map(resultSet);
+			room.setPlaylist(playlist);
+			room.map(resultSet);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return room;
+	}
+}
