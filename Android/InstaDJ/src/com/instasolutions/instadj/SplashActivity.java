c1dfc4636c4ec92f3e83f882dde716c54a44ea31
Eric Cameracci <e.cameracci@gmail.com>
2014-07-14 00:17:49 -0400

    Changed the progress animation

--- Android/InstaDJ/src/com/instasolutions/instadj/SplashActivity.java
+++ Android/InstaDJ/src/com/instasolutions/instadj/SplashActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
@@ -16,6 +17,9 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
         ImageView logoImage = (ImageView)findViewById(R.id.splash_image);
+        WebView loadAnimation = (WebView)findViewById(R.id.splash_load);
+        loadAnimation.loadUrl("file:///android_asset/splash.html");
+        loadAnimation.setBackgroundColor(0x00000000);
         logoImage.setImageResource(R.drawable.logo_animation);
         AnimationDrawable logoAnimation = (AnimationDrawable)logoImage.getDrawable();
         logoAnimation.start();
